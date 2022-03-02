2c970ae9c6b7cc5828d974971baaeb7d53d9fb0d
Eric Czajkowski <eric.czajkowski@gmail.com>
2022-03-01 21:51:02 -0500

    improvements

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
+const fs = require('fs').promises
 const axios = require('axios');
 const ical = require('ical-generator')
 
-// const calendar = require('./calendar.json')
+const run = async () => {
+  const { data } = await axios.get('https://api.phish.net/v5/shows/artist/phish.json?order_by=showdate&direction=desc&apikey=')
 
-axios.get('https://api.phish.net/v5/shows/artist/phish.json?order_by=showdate&direction=desc&apikey=')
-  .then(function (response) {
+  const calendar = ical({name: 'Phish'});
 
-    const shows = response.data.data
+  const shows = data.data
 
-    const calendar = ical({name: 'Phish'});
+  shows.forEach(show => {
 
-    shows.forEach(show => {
+    const {
+      showdate,
+      venue,
+      city,
+      state,
+      country,
+      permalink,
+    } = show
 
-      const showDate = new Date(show.showdate)
-      show.show_date
+    calendar.createEvent({
+      
+      // NOTE: dates are in UTC
+      start: new Date(showdate),
+      allDay: true,
 
-      calendar.createEvent({
-        start: showDate,
-        end: showDate,
-        summary: show.venue,
-        description: show.setlist_notes,
-        location: `${show.city}, ${show.state}`,
-        // url: 'http://sebbo.net/'
-      });
-    })
+      summary: venue,
 
-    console.info(calendar.toString())
+      // NOTE: description has some escaped characters
+      // description: show.setlist_notes,
+      location: [city, state, country].filter(l => !!l).join(', '),
 
+      // NOTE: Link to setlist (doesn't show up in Google)
+      url: permalink,
+    });
   })
+
+  console.info(calendar.toString())
+  await fs.writeFile('phish.ics', calendar.toString())
+}
+
+run()
